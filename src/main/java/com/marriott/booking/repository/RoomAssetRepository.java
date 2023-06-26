c1d22ecf68a6764a89d27fb1ecb75255112b17e9
vinylmatt <vinylmatt@users.noreply.github.com>
2023-06-26 15:04:34 +0100

    getting the booking asset from the type now.

--- src/main/java/com/marriott/booking/repository/RoomAssetRepository.java
+++ src/main/java/com/marriott/booking/repository/RoomAssetRepository.java
@@ -1,9 +1,18 @@
 package com.marriott.booking.repository;
 
+import com.marriott.booking.exception.GuestNotFoundException;
+import com.marriott.booking.exception.RoomNotFoundException;
+import com.marriott.booking.model.Guest;
 import com.marriott.booking.model.RoomAsset;
+import com.marriott.booking.model.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RoomAssetRepository extends JpaRepository<RoomAsset, Long>{
+public interface RoomAssetRepository extends JpaRepository<RoomAsset, Long> {
+    @Query("select a from RoomAsset a where a.roomType.id = ?1")
+    List<RoomAsset> findByRoomTypeId(Long roomType_id) throws RoomNotFoundException;
 }
