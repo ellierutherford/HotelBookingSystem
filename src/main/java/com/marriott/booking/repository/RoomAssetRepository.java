6e5f100c062e57965fdd6393b2d61b1bf27ca3de
vinylmatt <vinylmatt@users.noreply.github.com>
2023-06-28 08:37:03 +0100

    revert messing in file

--- src/main/java/com/marriott/booking/repository/RoomAssetRepository.java
+++ src/main/java/com/marriott/booking/repository/RoomAssetRepository.java
@@ -19,11 +19,8 @@ public interface RoomAssetRepository extends JpaRepository<RoomAsset, Long> {
     @Query("select a from RoomAsset a where a.roomType.id = ?1")
     List<RoomAsset> findByRoomTypeId(Long roomType_id) throws RoomNotFoundException;
 
-    @Query("SELECT e.id FROM RoomAsset e WHERE e.id NOT IN (SELECT g.id FROM Booking g WHERE (:start_date BETWEEN g.startDate and g.endDate or :end_date BETWEEN g.startDate and g.endDate))")
-    List<Long> findAvailableRooms(@Param("start_date") LocalDate start_date, @Param("end_date") LocalDate end_date);
-
-    @Query("SELECT e.id FROM RoomAsset e WHERE e.id NOT IN (SELECT g.id FROM Booking g WHERE (:start_date BETWEEN g.startDate and g.endDate or :end_date BETWEEN g.startDate and g.endDate))")
-    List<Long> findBookedRooms(@Param("start_date") LocalDate start_date, @Param("end_date") LocalDate end_date);
+    @Query("SELECT e FROM RoomAsset e WHERE e.id NOT IN (SELECT g.id FROM Booking g WHERE (:start_date BETWEEN g.startDate and g.endDate or :end_date BETWEEN g.startDate and g.endDate))")
+    List<RoomAsset> findAvailableRooms(@Param("start_date") LocalDate start_date, @Param("end_date") LocalDate end_date);
 
     @Query("SELECT e FROM RoomAsset e WHERE e.roomType.id = :roomType_id AND e.id NOT IN (SELECT g.id FROM Booking g WHERE (:start_date BETWEEN g.startDate AND g.endDate OR :end_date BETWEEN g.startDate AND g.endDate))")
     List<RoomAsset> findAvailableRoomsByType(@Param("start_date") LocalDate start_date, @Param("end_date") LocalDate end_date, @Param("roomType_id") Long roomType_id);
