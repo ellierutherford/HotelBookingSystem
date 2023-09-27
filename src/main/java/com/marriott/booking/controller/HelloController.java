77e7ffb3db0a582f7531509609a223fe1877e14c
matt <matt@vinyl-matt.com>
2023-09-27 12:42:47 +0100

    Auth working for the API

--- src/main/java/com/marriott/booking/controller/HelloController.java
+++ src/main/java/com/marriott/booking/controller/HelloController.java
@@ -1,6 +1,14 @@
 package com.marriott.booking.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+
+
 
 @RestController
 @RequestMapping("/api")
@@ -12,7 +20,22 @@ public class HelloController {
     }
 
     @PostMapping("/post")
-    public String postMessage(@RequestBody String message) {
-        return "Received message: " + message;
+    public String postMessage(@RequestBody String message, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = "";
+        if(authentication instanceof AnonymousAuthenticationToken){
+            username = "anon "+message ;
+        }
+        else{
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = "Auth" + message;
+        }
+
+
+
+
+        return "Received message: " + username;
     }
 }
